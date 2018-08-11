b7c9d679985cf2927163a2290d4bc56e0198284c
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-11 13:51:08 +0200

    Completed first part of the going intent

--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,48 @@
 const Alexa = require('alexa-sdk');
+const request = require('request-promise');
+
+const historyUrl = "http://54.191.146.40:8080/travel-history/";
+
+function addNextLocation(base){
+	return request({
+		url: historyUrl + "history/next",
+		transform: function (body){
+			console.log("Next:");
+			console.log(body);			
+			base.next = body == "" ? {} : JSON.parse(body);
+			return base;
+		}
+	})
+}
+
+function addCurrentLocation(base){
+	return request({
+		url: historyUrl + "history/current",
+		transform: function (body){
+			console.log("Current:");
+			console.log(body);
+			base.current = body == "" ? {} : JSON.parse(body);
+			return base;
+		}
+	})
+}
+
+function generateGoingResponse(data){
+	console.log("Complete:");
+	console.log(data);
+	if(data.next.name == undefined){
+		return "William hasn't decided where to go next, he's still in " + data.current.name + ".";
+	} else {
+		return "William is going to fill this in later.";
+	}
+}
 
 const handlers = {
     'going' : function(){
-        this.emit(':tell', "William is going to get this working!");
+		console.log("Finding out where William's going next..");
+		addNextLocation({})
+		.then(data => addCurrentLocation(data))
+		.then(data => this.emit(':tell', generateGoingResponse(data)));
     }
 }
 
